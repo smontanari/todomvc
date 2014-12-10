5251f399b16326f0d737ec4cfb7396860469ec0e
Silvio Montanari <montanari.silvio@gmail.com>
2014-12-10 16:09:07 +1100

    funcunit tests for emberjs

--- examples/emberjs/js/smocker-scenarios.js
+++ examples/emberjs/js/smocker-scenarios.js
@@ -26,8 +26,32 @@
     });
   });
 
+  smocker.scenario('no_todos', function() {
+    this.get('/todos').respondWith({content: {todos: []}});
+
+    this.put('/todos').respondWith(function(url, content, headers) {
+      if (content) {
+        console.log(content)
+        data = JSON.parse(content);
+        if (data.length == 1 && data[0].title != 'A Simple todo') {
+          return {
+            status: 400,
+            headers: {'Content-Type': 'text/plain'},
+            content: 'Error. Unexpected request content: ' + content
+          }
+        }
+      }
+      return {status: 204}
+    });
+  });
+
   smocker.groupScenarios('scenario1', ['static-fixture', 'store-data']);
   smocker.groupScenarios('scenario2', ['dynamic-fixture', 'store-data']);
 
-  smocker.play('scenario2');
+  var match = /[?&]test_scenario=(\w+)($|&.*)/.exec(window.location.search);
+  if (match) {
+    var scenarioName = match[1];
+    console.warn("Playing test scenario: " + scenarioName);
+    smocker.play(scenarioName);
+  }
 })();
